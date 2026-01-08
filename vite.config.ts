7cbe4afa934b02e00a92a439bee1fdd1fa57e30a
김의연 <naturalk@kyobobook.com>
2026-01-08 09:56:55 +0900

    chore: update pkg scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), 
+    // 플러그인 추가 (자동으로 tsconfig 경로 매핑)
+    tsconfigPaths(),
+  ],
 })
